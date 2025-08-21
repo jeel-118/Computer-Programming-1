6404700c9c1fdc9eb6e00ab285c54a832707b15f
jeel-118 <jeeltopper832049@gmail.com>
2025-08-21 22:15:13 +0530

    Add files via upload

---
+++ mathematics.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x=5;
+    int y=7;
+    int a=x+y;
+    printf("%d\n",a);
+    int b=y-x;
+    printf("%d\n",b);
+    int c=y/x;
+    printf("%d\n",c);
+    int d=x*y;
+    printf("%d\n",d);
+    return 0;
+}
