77439bcec7a74e950f40ed3eb1b6d296fce2ae00
jeel-118 <jeeltopper832049@gmail.com>
2025-08-24 22:29:03 +0530

    Add files via upload

---
+++ areaoftriangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int area;
+int height;
+printf("Enter height:");
+scanf("%d",&height);
+int length;
+printf("Enter length:");
+scanf("%d",&length);
+area=height*(length*0.5);
+printf("%d",area);
+return 0;
+}
