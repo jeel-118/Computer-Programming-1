dc5d71b8373ea672fbae30a214d5c4e58960d13a
jeel-118 <jeeltopper832049@gmail.com>
2025-08-24 21:56:32 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int area;
+int length;
+printf("Enter length:");
+scanf("%d",&length);
+int breath;
+printf("Enter breath:");
+scanf("%d",&breath);
+area =length*breath;
+printf("%d",area);
+return 0;
+}
