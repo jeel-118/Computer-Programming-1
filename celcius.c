acdc96af6591c2f791a70623a2155247d73b6bf1
jeel-118 <jeeltopper832049@gmail.com>
2025-08-24 19:46:58 +0530

    Add files via upload

---
+++ celcius.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+double fahrenheit;
+int celcius;
+printf("Enter celcius:");
+scanf("%d",&celcius);
+fahrenheit=(celcius*1.8)+32;
+printf("%f",fahrenheit);
+return 0;
+}
