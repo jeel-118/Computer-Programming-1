cb49c8596a886e55a969c5065752290f1173386a
jeel-118 <jeeltopper832049@gmail.com>
2025-08-27 19:28:13 +0530

    Add files via upload

---
+++ greatestoftwo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int a;
+printf("Enter a:");
+scanf("%d",&a);
+int b;
+printf("Enter b:");
+scanf("%d",&b);
+if(a>b){
+    printf("a will be greatest");
+}
+else{
+    printf("b will be greatest");
+}
+}
