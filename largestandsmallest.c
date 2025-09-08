25fb1e6bc3af344df6ee36d1e61c86bd5d12ac79
jeel-118 <jeeltopper832049@gmail.com>
2025-09-08 16:20:11 +0530

    Add files via upload

---
+++ largestandsmallest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter n:");
+scanf("%d",&n);
+int a;
+printf("Enter a:");
+scanf("%d",&a);
+for(int i=a;i<=n;i++){
+
+}
+printf("The largest number is:%d\n",n);
+printf("The smallest number is:%d\n",a);
+return 0;
+}
